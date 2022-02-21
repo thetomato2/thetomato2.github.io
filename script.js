dce365f5a73595e08cf52f4a2487631b3e510c02
thetomato2 <derekj.nord@gmail.com>
2022-02-20 21:08:42 -0600

    do do da do do

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hello, world!");
+
+let x = 5;
+
+
+console.log("x");
+console.log(x);
+
+console.log("x * 2");
+console.log(x * 2);
+
+console.log("x / 2");
+console.log(x / 2);
+
+console.log("x % 2");
+console.log(x % 2);
+
+console.log("x ^ 2");
+console.log(x ** 2);
+
+let y = "5";
+
+console.log(x == y);
+console.log(x === y);
